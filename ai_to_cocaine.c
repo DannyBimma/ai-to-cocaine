20e753dad6981d3928c1027870e868473f276fec
DannyBimma <dantrotman92@gmail.com>
2025-06-06 18:33:15 -0400

    Init main with necessary pointers and prep to write to file.

--- ai_to_cocaine.c
+++ ai_to_cocaine.c
@@ -6,3 +6,15 @@
 #define TARGET_STRING "AI"
 #define REPLACEMENT_STRING "cocaine"
 #define OUTPUT_FILE "the_cocaine_file.txt
+
+int main(void) {
+    char buffer[BUFFER_SIZE];
+    char* replaced_text;
+    FILE* output_file;
+
+    output_file = fopen(OUTPUT_FILE, "w");
+    if (output_file == NULL) {
+        fprintf(stderr, "FAILED SUCCESSFULLY: Could not create output!\n");
+        return 69;
+    }
+}
