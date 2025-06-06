753f1d69cdbdaf04de79ea9a594e7a6178078623
DannyBimma <dantrotman92@gmail.com>
2025-06-06 18:21:07 -0400

    Do the includes and defines.

---
+++ ai_to_cocaine.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 4096 // Round about 40kb should do
+#define TARGET_STRING "AI"
+#define REPLACEMENT_STRING "cocaine"
+#define OUTPUT_FILE "the_cocaine_file.txt
